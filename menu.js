3b23aa6a5ba6fec77093dc247424046c059ae810
KaraKim <kkim6156@gmail.com>
2020-09-02 00:58:26 -0400

    Added face

--- menu.js
+++ menu.js
@@ -5,11 +5,3 @@ $('.navTrigger').click(function () {
 
 });
 
-// $(window).scroll(function() {
-//     if ($(document).scrollTop() > 50) {
-//         $('.nav').addClass('affix');
-//         console.log("OK");
-//     } else {
-//         $('.nav').removeClass('affix');
-//     }
-// });
