faa47e61174646ce580ea4c8782ba8b184daa9c8
KaraKim <kkim6156@gmail.com>
2020-09-11 07:34:37 -0400

    Testing size

--- menu.js
+++ menu.js
@@ -12,3 +12,12 @@ $('.navTrigger').click(function () {
 //         $('.nav').removeClass('affix');
 //     }
 // });
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
